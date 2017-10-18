31883cb385c869560791e201501ec2e4b8bb5d04
MarcoAc93 <maandrad@compusoluciones.com>
2017-10-18 11:50:22 -0500

    cambios

--- facilito-api/config/secrets.js
+++ facilito-api/config/secrets.js
@@ -1,8 +1,8 @@
 module.exports = {
 	cloudinary: {
-		api_key: '',
-		cloud_name: '',
-		api_secret: ''
+		api_key: '289121369369247',
+		cloud_name: 'marco-ac-93',
+		api_secret: '3aYvCITsg_2GwiiMMYzREtB5wxA'
 	},
 	jwtSecret: 'rasho_laserMamaL0n'
 }
